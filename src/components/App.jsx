17e98efbdb3982bdcf8e5336080b347b68e02a70
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-02-22 00:02:56 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics'
 import FriendList from './Friends/Friends';
+import TransactionHistory from './Transactions/Transactions';
 
 import user from '../userData/user.json'
 import data from '../statisticsData/data.json'
 import friends from '../friendsData/friends.json'
+import transactions from '../transactionsData/transactions.json'
 // console.log(statistics);
 
 
@@ -33,6 +35,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -21,4 +21,8 @@ function FriendList(friends) {
 
 export default FriendList
 
+// FriendList.propTypes = {
+
+// }
+
 // avatar: 'https://cdn-icons-png.flaticon.com/512/1998/1998592.png', name: 'Mango', isOnline: true, id: 1812
