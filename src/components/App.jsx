3762976c6d7477739f7ff2e766223a171a1e540a
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-02-21 08:37:46 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import user from '../userData/user.json'
+console.log(user);
+
+// import  Profile  from './Profile/Profile';
+// console.log(Profile);
+
 export const App = () => {
   return (
     <div
@@ -10,7 +16,26 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      {/* <Profile/> */}
     </div>
   );
 };
+
+{/* <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/> */}
+
+// Profile.propTypes ={
+//   username: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   stats: PropTypes.shape({
+//     followers: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//   })
+// }
