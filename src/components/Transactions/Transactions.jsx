54202d5be19aa5005d0295f59704a1b6b07f528c
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-02-22 09:47:35 +0200

    task 4

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,28 +1,28 @@
 
 
-{/* <table class="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table> */}
-
-function TransactionHistory(items) { }
+function TransactionHistory(items) {
+    console.log(items.items);
+    return (
+        <table class="transaction-history">
+            <thead>
+                <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+                </tr>
+            </thead>
+            <tbody>
+                {items.items.map(({ id,type,amount,currency }) => {
+                    return (<tr key={id}>
+                        <td>{ type }</td>
+                        <td>{ amount }</td>
+                        <td>{ currency }</td>
+                    </tr>)
+                })}
+            </tbody>
+            </table>
+    )
+ }
 
 export default TransactionHistory
+
